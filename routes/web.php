010ba53f7ac164f89a570e61c62bc3a48ed5d54c
TaterLi <admin@lijingquan.net>
2021-04-09 02:54:38 -0400

    用户个人页面原型

--- routes/web.php
+++ routes/web.php
@@ -17,3 +17,11 @@ Route::get('/', 'PagesController@root')->name('root');
 
 //Laravel的认证路由可以从vendor/laravel/ui/src/AuthRouteMethods.php找得到!
 Auth::routes(['verify' => true]);
+
+Route::resource('users', 'UsersController', ['only' => ['show', 'update', 'edit']]);
+/*
+上面代码等效为如下,使用resource是可以很方便符合RESETful原则.
+Route::get('/users/{user}', 'UsersController@show')->name('users.show');
+Route::get('/users/{user}/edit', 'UsersController@edit')->name('users.edit');
+Route::patch('/users/{user}', 'UsersController@update')->name('users.update');
+*/
