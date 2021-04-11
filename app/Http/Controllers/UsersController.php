ddfa85155f18f398578bc7944b6b7be06f7e24fb
TaterLi <admin@lijingquan.net>
2021-04-11 04:21:27 -0400

    编辑个人资料

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -4,11 +4,21 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\User;
+use App\Http\Requests\UserRequest;
 
 class UsersController extends Controller
 {
     //
     public function show(User $user){
         return view('users.show',compact('user'));
+    }
+
+    public function edit(User $user){
+        return view('users.edit',compact('user'));
+    }
+
+    public function update(UserRequest $request,User $user){
+        $user->update($request->all()); //本来想引入$user的不行,实际上需要引入所有传递的参数.
+        return redirect()->route('users.show',$user->id)->with('success','个人资料更新成功!');
     }
 }
