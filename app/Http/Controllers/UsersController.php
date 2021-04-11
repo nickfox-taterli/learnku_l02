7a09acc7a2054fcae79e4005380f65f5affe6f79
TaterLi <admin@lijingquan.net>
2021-04-11 04:36:15 -0400

    上传头像

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -6,6 +6,9 @@ use Illuminate\Http\Request;
 use App\Models\User;
 use App\Http\Requests\UserRequest;
 
+//引入一下自己创造的Handle.
+use App\Handlers\ImageUploadHandler;
+
 class UsersController extends Controller
 {
     //
@@ -17,8 +20,19 @@ class UsersController extends Controller
         return view('users.edit',compact('user'));
     }
 
-    public function update(UserRequest $request,User $user){
-        $user->update($request->all()); //本来想引入$user的不行,实际上需要引入所有传递的参数.
+    public function update(UserRequest $request,ImageUploadHandler $uploader,User $user){
+        $data = $request->all();
+
+        //头像要储存起来的嘛~
+        if ($request->avatar) {
+            $result = $uploader->save($request->avatar, 'avatars', $user->id);
+            //如果上面返回false就是没成功,估计是扩展名不允许.
+            if ($result) {
+                $data['avatar'] = $result['path'];
+            }
+        }
+
+        $user->update($data); //本来想引入$user的不行,实际上需要引入所有传递的参数.
         return redirect()->route('users.show',$user->id)->with('success','个人资料更新成功!');
     }
 }
