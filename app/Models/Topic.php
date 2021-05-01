8478e4385142199a152d845da7e189ca4025f614
TaterLi <admin@lijingquan.net>
2021-05-01 11:10:37 -0400

    话题列表页面,并优化主题外观.

--- app/Models/Topic.php
+++ app/Models/Topic.php
@@ -9,4 +9,13 @@ class Topic extends Model
     use HasFactory;
     
     protected $fillable = ['title', 'body', 'user_id', 'category_id', 'reply_count', 'view_count', 'last_reply_user_id', 'order', 'excerpt', 'slug'];
+
+    // 设置从属关系
+    public function category(){
+        return $this->belongsTo(Category::class);
+    }
+
+    public function user(){
+        return $this->belongsTo(User::class);
+    }
 }
